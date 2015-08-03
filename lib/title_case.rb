2d109f9bb17ab8f3e9d144c2e2926b901514f522
Guest User <student@epicodus.com>
2015-08-03 11:41:00 -0700

    First word always capitalized

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -3,9 +3,13 @@ class String
     designated_words = ["the", "a", "an", "and", "to", "of", "on", "at"]
     old_array = self.split(" ")
     old_array.each() do |word|
-      if designated_words.include?(word)
-      else
+      if old_array[0].eql?(word)
         word.capitalize!()
+      else
+        if designated_words.include?(word)
+        else
+          word.capitalize!()
+        end
       end
     end
     old_array.join(" ")
